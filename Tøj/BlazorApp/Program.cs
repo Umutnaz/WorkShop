6d9c8ceaadd25f6beda759665186bd724c0f688c
Umutnaz <umutnaz@outlook.dk>
2025-04-04 13:30:07 +0200

    Der sadt en masse ind fra oles, nøgle ord skal bare ændres. Ved ikke lige hvorfor blazor også siger fejl men det er små ting

--- Tøj/BlazorApp/Program.cs
+++ Tøj/BlazorApp/Program.cs
@@ -1,11 +1,19 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tøj;
+using Tøj.Services;
+using Tøj.Services.Login;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddSingleton<IClothingServices, ClothingServicesMock>();
+builder.Services.AddScoped<ILoginService, LoginServiceClientSide>();
 
 await builder.Build().RunAsync();
---
+++ Tøj/BlazorApp/Services/Login/LoginServiceClientSide.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Blazored.LocalStorage;
+using Core;
+
+namespace Tøj.Services.Login;
+
+public class LoginServiceClientSide : ILoginService  {
+    
+    private ILocalStorageService localStorage { get; set; }
+    
+    public LoginServiceClientSide(ILocalStorageService ls) {
+        localStorage = ls;
+    }
+    public async Task<User?> GetUserLoggedIn() {
+        User? res = await localStorage.GetItemAsync<User>("user");
+        return res;
+    }
+    public async Task<bool> Login(string username, string password)
+    {
+        User? u = await Validate(username, password);
+        if (u != null)
+        {
+            u.Password = "validated";
+            await localStorage.SetItemAsync("user", u);
+            return true;
+        }
+        return false;
+    }
+
+    private List<User> users = new List<User>()
+    {
+        new User { Id = 1, UserName = "rip",Password = "1234", Role = "admin"},
+        new User {Id=2, UserName = "rap", Password = "4321", Role = "admin"},
+        new User {Id = 3, UserName = "rup", Password = "qwerty", Role="member"}
+    };
+    protected virtual async Task<User?> Validate(string username, string password)
+    {
+        foreach (User u in users)
+
+            if (username.Equals(u.UserName) && password.Equals(u.Password))
+                return u;
+
+        return null;
+    }
+}
