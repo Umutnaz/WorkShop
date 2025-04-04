d89990f78a783d50aa62b674b37f254d26799192
Umutnaz <umutnaz@outlook.dk>
2025-04-04 13:07:26 +0200

    Jeg har lavet lidt i laaneside, og lavet references til core. Der er fejl, men det skal justeres med modelklasserne når de nu bliver færdige

--- Tøj/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ Tøj/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2abc804ac7768a7235e55b7250f2cd58f273ea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
