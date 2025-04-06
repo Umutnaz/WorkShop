551fb87ec9578bd6d26b7534041f1a080bec8cc3
MortenStaunstrup <mort490fgromling@gmail.com>
2025-04-06 12:23:34 +0200

    Tilføjet user login page og CreateUserPage.razor

--- WorkShoppen/Program.cs
+++ WorkShoppen/Program.cs
@@ -2,13 +2,18 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WorkShoppen;
+using WorkShoppen.Service;
+using WorkShoppen.Service.Interfaces;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<ILoginService, LoginServiceClientside>();
+builder.Services.AddSingleton<IClothingService, ClothingServiceMock>();
 
 await builder.Build().RunAsync();
--- WorkShoppen/Service/LoginServiceClientside.cs
+++ WorkShoppen/Service/LoginServiceClientside.cs
@@ -6,6 +6,26 @@ namespace WorkShoppen.Service;
 
 public class LoginServiceClientside : ILoginService
 {
+
+    private List<User> users = new List<User>()
+    {
+        new User()
+        {
+            UserId = 1, Username = "Tiffany", Password = "Tiff123", Email = "whatever@gmail.com", Role = "admin",
+            PhoneNumber = 125235
+        },
+        new User()
+        {
+            UserId = 2, Username = "Beth", Password = "Kittens", Email = "b@hotmail.com", Role = "user",
+            PhoneNumber = 41424
+        },
+        new User()
+        {
+            UserId = 3, Username = "Camilla", Password = "123", Role = "user", Email = "c@hotmail.com",
+            PhoneNumber = 41425
+        }
+    };
+    
     private ILocalStorageService localStorage {get;set;}
 
     public LoginServiceClientside(ILocalStorageService localStorage)
@@ -21,14 +41,32 @@ public class LoginServiceClientside : ILoginService
 
     public async Task<bool> Login(string username, string password)
     {
-        if (username.Equals("peter") && password.Equals("123"))
+        if (!string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(password))
         {
-            User user = new User() { Name = username, Password = password };
-            
-            await localStorage.SetItemAsync("user", user);
-            return true;
+            // Check om username og password combination eksisterer
+            // I såfal, log bruger ind
+            if (users.Any(x => x.Username == username && x.Password == password))
+            {
+                User user = new User() { Username = username, Password = password };
+                
+                await localStorage.SetItemAsync("user", user);
+                return true;
+            }
+           
+            Console.WriteLine("Username or password is incorrect");
+            return false;
         }
 
         return false;
+    }
+
+    public void CreateUser(User user)
+    {
+        users.Add(user);
+    }
+
+    public int GetUserIdMax()
+    {
+        return users.Max(x => x.UserId);
     }
 }
