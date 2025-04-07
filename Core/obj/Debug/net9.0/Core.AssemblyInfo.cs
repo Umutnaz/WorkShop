4e2b977cb7c8a2e9554549b4c4b8856ad119ef51
eaa24 <eaa24imha@students.eaaa.dk>
2025-04-07 12:49:28 +0200

    css

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e7d2a4f9a9696a1a9cb22c40673743977070d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d20a775f08eb581aa3c056ab6dbf36ffa5dbae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
